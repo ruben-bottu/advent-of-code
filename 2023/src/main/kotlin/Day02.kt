863a285a94e98270f5e103bfa0fb3e40f3749074
Ruben Bottu <ruben.bottu@qmino.com>
2023-12-16 21:33:14 +0100

    Solved 2023 Day 2 part 2

--- 2023/src/main/kotlin/Day02.kt
+++ 2023/src/main/kotlin/Day02.kt
@@ -6,7 +6,7 @@ val colorMax = mapOf(
     "blue" to 14
 )
 
-fun toCubes(line: String) =
+fun toNestedCubes(line: String) =
     line.split(":", ";")
         .drop(1)
         .map {
@@ -17,9 +17,33 @@ fun toCubes(line: String) =
         }
 
 fun isPossible(line: String) =
-    toCubes(line).all {
+    toNestedCubes(line)
+        .all {
             it.all { (amount, color) -> amount <= colorMax.getValue(color) }
         }
+
+// ====================================================================
+
+fun toCubes(line: String) = line
+    .split(":", ";", ",")
+    .drop(1) // drop "Game X"
+    .map { it.trimStart() }
+    .map { it.split(" ") }
+    .map { (amount, color) -> Cube(amount.toInt(), color) }
+
+val colorToZero = mapOf(
+    "red" to 0,
+    "green" to 0,
+    "blue" to 0
+)
+
+fun maxAmountOfColors(cubes: List<Cube>) =
+    cubes.fold(colorToZero) { acc, (amount, color) ->
+        if (amount > acc.getValue(color)) acc + Pair(color, amount) else acc
+    }
+
+fun power(colorMap: Map<String, Int>) =
+    colorMap.values.reduce { acc, amount -> acc * amount }
 
 fun main() {
     fun partOne(input: List<String>) =
@@ -27,12 +51,16 @@ fun main() {
             if (isPossible(current)) acc + (index + 1) else acc
         }
 
-    fun partTwo(input: List<String>): Int {
-        return 0
-    }
+    fun partTwo(input: List<String>) = input
+        .map { toCubes(it) }
+        .map { maxAmountOfColors(it) }
+        .sumOf { power(it) }
+
 
     val input = readInput("Day02")
-    //isPossible(input[0])
+    //isPossible(input.first())
+    //toCubes(input.first()).println()
     partOne(input).println()
-    //partTwo(input).println()
+    partTwo(input).println()
+    //maxAmountOfColors(input[2]).println()
 }
